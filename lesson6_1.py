4a891f87847a3171da08879e980620683913b143
Betty <ss030ss03tw@gmail.com>
2022-11-12 14:06:46 +0800

    def

--- lesson6_1.py
+++ lesson6_1.py
@@ -1,6 +1,12 @@
 #建立自訂的功能
-def sayHello(): #function名稱是sayHello
+
+#function名稱是sayHello
+def sayHello(): #沒有參數的function
     print("Hello!")
 
+def sayHello_withName(name):  #有一個參數的function
+        print(f"Hello {name}!")
+
+
 if __name__ == "__main__":
-    sayHello()
+    sayHello_withName("Betty") #引數值傳遞至name參數內
